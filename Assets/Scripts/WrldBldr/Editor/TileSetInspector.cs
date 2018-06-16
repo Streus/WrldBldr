f03ae4b1060097cab848c00160ca68afcdd5f6ea
Sam Streed <embersteelstreus@gmail.com>
2018-06-16 14:03:46 -0500

    Added SetDirty call to TileSetInspector. Hopefully no more clearing.

--- Assets/Scripts/WrldBldr/Editor/TileSetInspector.cs
+++ Assets/Scripts/WrldBldr/Editor/TileSetInspector.cs
@@ -40,6 +40,10 @@ public class TileSetInspector : Editor
 		}
 		EditorGUILayout.EndFadeGroup ();
 
-		obj.ApplyModifiedProperties ();
+		if (GUI.changed)
+		{
+			obj.ApplyModifiedProperties ();
+			EditorUtility.SetDirty (target);
+		}
 	}
 }
